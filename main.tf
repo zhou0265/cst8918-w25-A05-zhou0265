0cc05765ba5c0347c584c0881d3c8817d3e23263
zhou0265 <zhou0265@algonquinlive.com>
2025-02-07 00:19:57 -0500

    Define resources-Virtual Network Interface Card (NIC)

--- main.tf
+++ main.tf
@@ -100,3 +100,17 @@ resource "azurerm_network_security_group" "nsg" {
     destination_address_prefix = "*"
   }
 }
+
+# 4.7 Virtual Network Interface Card (NIC)
+resource "azurerm_network_interface" "nic" {
+  name                = "${var.labelPrefix}-A05-NIC"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
