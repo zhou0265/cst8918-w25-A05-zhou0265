c90dcc9e03d1465b02bca4a0232a442f0b2a0c56
zhou0265 <zhou0265@algonquinlive.com>
2025-02-07 00:22:09 -0500

    Define resources-Virtual Machine

--- main.tf
+++ main.tf
@@ -131,4 +131,32 @@ data "cloudinit_config" "init" {
     content      = file("${path.module}/init.sh")
   }
 }
+# 4.10 Virtual Machine
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "${var.labelPrefix}-A05-VM"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = var.admin_username
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  custom_data = data.cloudinit_config.init.rendered
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
 
