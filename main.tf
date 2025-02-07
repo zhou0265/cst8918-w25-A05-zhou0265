e6182990909c27e947e3dbe7e7b0d369944a9890
zhou0265 <zhou0265@algonquinlive.com>
2025-02-07 00:18:17 -0500

    Define resources-subnet

--- main.tf
+++ main.tf
@@ -62,3 +62,10 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
 }
+# 4.5 Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.labelPrefix}-A05-Subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
