152c7885a05d7c7bc6e590397ae8c7af33f059ea
zhou0265 <zhou0265@algonquinlive.com>
2025-02-07 00:22:50 -0500

    Define resources-Output Values

--- main.tf
+++ main.tf
@@ -160,3 +160,11 @@ resource "azurerm_linux_virtual_machine" "vm" {
   }
 }
 
+# Output Values
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "public_ip" {
+  value = azurerm_public_ip.public_ip.ip_address
+}
